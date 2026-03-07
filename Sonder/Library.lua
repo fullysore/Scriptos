ac7bd97142d626e3af09e4c43427a5eb0bc391de
fullysore <jedithewalkingwusa@gmail.com>
2026-03-07 01:06:14 -0400

    Update Library.lua

--- Sonder/Library.lua
+++ Sonder/Library.lua
@@ -1710,7 +1710,7 @@ local Library do
                     Name = "\0",
                     BorderColor3 = FromRGB(0, 0, 0),
                     BackgroundTransparency = 1,
-                    Position = (Items["Text"].Instance and #Items["Text"].Instance:GetChildren() >= 1) and UDim2.new(0, 194, 0, 0) or UDim2.new(0, 215, 0, 0),
+                    Position = (#Items["Text"].Instance:GetChildren() > 0) and UDim2.new(0, 194, 0, 0) or UDim2.new(0, 215, 0, 0),
                     Size = UDim2New(0, 0, 1, 0),
                     BorderSizePixel = 0,
                     AutomaticSize = Enum.AutomaticSize.X,
