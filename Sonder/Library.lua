9ff6e31d4a247df0814fca89fee81b3f082658ad
fullysore <jedithewalkingwusa@gmail.com>
2026-04-06 13:58:51 -0400

    Update Library.lua

--- Sonder/Library.lua
+++ Sonder/Library.lua
@@ -20,7 +20,7 @@ local Library do
     local CoreGui = cloneref and cloneref(game:GetService("CoreGui")) or game:GetService("CoreGui")
     local TweenService = game:GetService("TweenService")
 
-    gethui = function()
+    gethui = gethui or function()
         return CoreGui
     end
 
